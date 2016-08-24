98b00384a79a7a76bc938c3b8964d44cc0d7112b
tjacek <tjacek@student.agh.edu.pl>
2016-08-24 12:20:01 +0200

    no empty string in tokens

--- python/parser.py
+++ python/parser.py
@@ -3,7 +3,10 @@ import re
 def tokenize(raw_str):
     whitespace= r"(\s)+"
     clean_str=re.sub(whitespace," ",raw_str)
-    clean_str=clean_str.replace("(","( ").replace(")"," )")
-    return clean_str.split(" ")
+    clean_str=clean_str.replace(" (","(").replace(") ",")")
+    clean_str=clean_str.replace("("," ( ").replace(")"," )")
+    tokens=clean_str.split(" ")
+    tokens.pop(0)
+    return tokens
 
 print(tokenize("(begin (define r 10)   (* pi (* r r)))"))   
