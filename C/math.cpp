67b62a35976d2a7973614760a52a4b87d9e73334
tjacek <tjacek@agh.edu.pl>
2022-02-22 21:49:52 +0100

    math regex

--- C/math.cpp
+++ C/math.cpp
@@ -14,18 +14,23 @@ void parse(std::string in_path){
   std::ifstream infile(in_path);
   std::string line;
   while (std::getline(infile, line)){
-//    std::cout << line << "\n";
     tokenize(line);
   }
 }
 
 std::vector<Token> tokenize(std::string line){
-  std::sregex_token_iterator iter(line.begin(),line.end(),ws,-1);
+  std::sregex_token_iterator iter(line.begin(),line.end(),ws_reg,-1);
   std::sregex_token_iterator end;
   std:: vector<Token> tokens;
   for ( ; iter != end; ++iter){
     std::string str_i=*iter;
-    std::cout << str_i <<"$";
+    if(std::regex_match (str_i,number_reg)){
+      std::cout << str_i << std::endl;
+    } else if(std::regex_match (str_i,var_reg)){
+      std::cout << str_i << std::endl;
+    } else if(std::regex_match (str_i,command_reg)){
+      std::cout << str_i << std::endl;
+    }
   }
   std::cout << std::endl;
   return tokens;
--- C/math.h
+++ C/math.h
@@ -9,7 +9,11 @@
 
 enum TokenType {NUMBER, SET,READ,PRINT,VAR};
 
-const static std::regex ws("\\s+");
+const static std::regex ws_reg("\\s+");
+const static std::regex number_reg("-?\\d+(\\.\\d+)?");
+const static std::regex command_reg("[\\+\\-\\*\\/=()]|set|read|print/");
+const static std::regex var_reg("[a-zA-Z][a-zA-Z0-9]*");
+
 
 class Token{
   TokenType type;
