f5925ac84d770c572ae089db505f4de5c04dd101
tjacek <tjacek@agh.edu.pl>
2022-02-22 02:49:23 +0100

    tokenize math

--- C/math.cpp
+++ C/math.cpp
@@ -10,6 +10,28 @@ Token::Token(TokenType type){
 	this->value=0.0;
 }
 
+void parse(std::string in_path){
+  std::ifstream infile(in_path);
+  std::string line;
+  while (std::getline(infile, line)){
+//    std::cout << line << "\n";
+    tokenize(line);
+  }
+}
+
+std::vector<Token> tokenize(std::string line){
+  std::sregex_token_iterator iter(line.begin(),line.end(),ws,-1);
+  std::sregex_token_iterator end;
+  std:: vector<Token> tokens;
+  for ( ; iter != end; ++iter){
+    std::string str_i=*iter;
+    std::cout << str_i <<"$";
+  }
+  std::cout << std::endl;
+  return tokens;
+}
+
 int main(){
-  Token a(5.0);
+  //Token a(5.0);
+  parse("test.math");
 }
--- C/math.h
+++ C/math.h
@@ -1,11 +1,15 @@
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <regex>
 #include <list>
 #ifndef MATH
 #define MATH
 
 enum TokenType {NUMBER, SET,READ,PRINT,VAR};
+
+const static std::regex ws("\\s+");
 
 class Token{
   TokenType type;
@@ -16,4 +20,5 @@ class Token{
     Token(TokenType type);
 };
 
+std::vector<Token> tokenize(std::string line);
 #endif
