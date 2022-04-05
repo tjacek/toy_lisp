e9b027cf83db4618db2a52b529ec16cbb88014c8
tjacek <tjacek@agh.edu.pl>
2022-04-05 19:41:32 +0200

    print types

--- C/tokens.cpp
+++ C/tokens.cpp
@@ -1,6 +1,91 @@
 #include "tokens.h"
 
+void interpret(std::string in_path){
+  std::ifstream infile(in_path);
+  std::string line;
+  std::vector<TokenSeqPtr> lines;
+  int line_counter=0;
+  while (std::getline(infile, line)){
+    line_counter++;
+    TokenSeqPtr tokens= tokenize(line);
+    tokens->print_types();
+/*    try{
+      StatementPtr stat_i= parse_statement(tokens);
+      eval_statment(stat_i,envir);
+    }catch(std::string e){
+       std::cout << "Line: " << line_counter << std::endl;
+       std::cout << e << std::endl;
+       return ; 
+    }*/
+    lines.push_back(tokens);
+  }
+}
+
+TokenSeqPtr tokenize(std::string line){
+  replace_string(line,"(","  ( ");
+  replace_string(line,")"," ) ");
+
+  TokenSeqPtr tokens=TokenSeqPtr(new TokenSeq());
+  std::sregex_token_iterator iter(line.begin(),line.end(),ws_reg,-1);
+  std::sregex_token_iterator end;
+
+  for ( ; iter != end; ++iter){
+    std::string str_i=*iter;
+    TokenType type_i;
+    if(std::regex_match (str_i,command_reg)){
+      type_i= SYNTAX;
+    } else if(std::regex_match (str_i,number_reg)){
+      type_i= NUMBER;
+    } else if(std::regex_match (str_i,var_reg)){
+      type_i= VAR;
+    }
+    TokenPtr token_i= TokenPtr(new Token(type_i,str_i));
+    tokens->add(token_i);
+  }
+  return tokens;
+}
+
+void replace_string(std::string& subject, const std::string& search,
+                          const std::string& replace) {
+    size_t pos = 0;
+    while((pos = subject.find(search, pos)) != std::string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }
+}
+
 Token::Token(TokenType type,std::string str){
-  this->type=VAR;
+  this->type=type;
   this->data=str;
+}
+
+std::string Token::get_type(){
+  switch(this->type){
+      case TokenType::NUMBER: return "Number";
+      case TokenType::VAR: return "Variable";
+      default: return this->data;
+  }
+}
+
+void TokenSeq::add(TokenPtr token){
+  this->tokens.push_back(token);
+}
+
+void TokenSeq::print(int i){
+  for (auto it = this->tokens.begin()+i; it != this->tokens.end(); ++it) {
+    TokenPtr token_j=(*it);
+    std::cout << token_j->data << " ";
+  }
+  std::cout << std::endl;
+}
+
+void TokenSeq::print_types(){
+  for(auto it = this->tokens.begin(); it != this->tokens.end(); ++it) {
+    std::cout <<  (*it)->get_type() <<" ";
+  }
+  std::cout << std::endl;
+}
+
+int main(){
+  interpret("test.lisp");
 }
--- C/tokens.h
+++ C/tokens.h
@@ -1,14 +1,29 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <regex>
+#include <memory>
+#include <algorithm>
 #ifndef MATH
 #define MATH
 
 enum TokenType {SYNTAX,NUMBER,VAR,STRING};
+
+const static std::regex ws_reg("\\s+");
+const static std::regex number_reg("-?\\d+(\\.\\d+)?");
+const static std::regex command_reg("[\\+\\-\\*\\/=()]|lambda|define");
+const static std::regex var_reg("[a-zA-Z][a-zA-Z0-9\\-]*");
 
 class Token{
   public:
     TokenType type;
     std::string data;
     Token(TokenType type,std::string str);
+    std::string get_type();
 };
+
+typedef std::shared_ptr<Token> TokenPtr;
 
 class TokenSeq{
   private:
@@ -16,11 +31,13 @@ class TokenSeq{
     int current=0;
 
   public:
-  	print(int i=0);
+  	void add(TokenPtr token);
+  	void print(int start=0);
   	void print_types();
 };
 
 typedef std::shared_ptr<TokenSeq> TokenSeqPtr;
 TokenSeqPtr tokenize(std::string line);
-
+void replace_string(std::string& subject, const std::string& search,
+	const std::string& replace);
 #endif
