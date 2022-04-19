f55df812e7004ae07a77c9cc5e98540ffe4090d1
tjacek <tjacek@agh.edu.pl>
2022-04-20 01:29:40 +0200

    print tokens

--- rust/math/src/main.rs
+++ rust/math/src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate lazy_static;
 use regex::Regex;
 
 enum TokenType{
@@ -16,23 +18,40 @@ struct TokenSeq {
   current: u32,
 }
 
+impl TokenSeq {
+  fn print(&self){
+    for token_i in &self.tokens{
+      print!("{} ",token_i.data);
+    }
+  }
+}
+
 fn tokenize(line:String) -> TokenSeq{
   let mut tokens_seq = TokenSeq{
      tokens: Vec::new(),
      current: 0
   };
+  lazy_static! {
+    static ref NUMBER_RE: Regex = Regex::new("-?\\d+(\\.\\d+)?").unwrap();
+    static ref SYNTAX_RE: Regex = Regex::new("[\\+\\-\\*=()]|set|read|print").unwrap();
+  }
   let splits = line.split_whitespace().into_iter();
   for split_i in splits {
-    let token_i=Token{token_type:TokenType::VAR, 
+    let mut type_i:TokenType=TokenType::VAR;
+    if NUMBER_RE.is_match(split_i){
+      type_i=TokenType::NUMBER;
+    }else if SYNTAX_RE.is_match(split_i){
+      type_i=TokenType::SYNTAX;
+    }
+    let token_i=Token{token_type:type_i, 
                       data:String::from(split_i)};
     tokens_seq.tokens.push(token_i);
-    println!("\"{}\"", split_i);
   }
   tokens_seq
 }
 
 fn main() {
   let hello = String::from("read miles");
-
-  tokenize(hello);
+  let tokens = tokenize(hello);
+  tokens.print();
 }
